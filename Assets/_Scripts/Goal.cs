e6f002a6a053a61d034087b3d6723d0dcdf0c009
Nishant Shastri <nishantshastri@Nishants-MacBook-Pro.local>
2019-10-02 16:23:12 -0400

    Castle and UI

---
+++ Assets/_Scripts/Goal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    static public bool goalMet = false;
+    // Start is called before the first frame update
+    void OnTriggerEnter( Collider other )
+    {
+        if ( other.gameObject.tag == "Projectile")
+        {
+            Goal.goalMet = true;
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_Scripts/Slingshot.cs
+++ Assets/_Scripts/Slingshot.cs
@@ -17,7 +17,17 @@ public class Slingshot : MonoBehaviour
 
     private Rigidbody projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     private void Awake() {
+        S = this;
         Transform launchPointTrans = transform.FindChild("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
